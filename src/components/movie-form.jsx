48cc5fb0c85b7eaa74cf12ba26167a1f308681d6
Thilanka Rodrigo <65930513+Thilanka0824@users.noreply.github.com>
2022-10-05 18:45:59 -0700

    done 2.0

--- src/components/movie-form.jsx
+++ src/components/movie-form.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { useState } from 'react'
 
 const MovieForm = () => {
-    const [title, setTitle] = useState("")
-    const [director, setDirector] = useState("")
-    const [actors, setActors] = useState("")
-    const [plot, setPlot] = useState("")
+    const [title, setTitle] = useState("");
+    const [director, setDirector] = useState("");
+    const [actors, setActors] = useState("");
+    const [plot, setPlot] = useState("");
+    const [year, setYear] = useState(0);
+    const [imbdRating, setRating] = useState(0);
 
     console.log(title)
     return (
@@ -15,26 +17,38 @@ const MovieForm = () => {
             <input type="text" onChange={(event) => {
                 setTitle(event.target.value)
             }} />
-            <br/>
+            <br />
             <label>Director:</label>
             <input type="text" onChange={(event) => {
                 setDirector(event.target.value)
             }} />
-            <br/>
+            <br />
             <label>Actors:</label>
             <input type="text" onChange={(event) => {
                 setActors(event.target.value)
             }} />
-            <br/>
+            <br />
             <label>Plot:</label>
             <input type="text" onChange={(event) => {
                 setPlot(event.target.value)
             }} />
-            
+            <br />
+            <label>Year:</label>
+            <input type="number" onChange={(event) => {
+                setYear(event.target.value)
+            }} />
+            <br />
+            <label>imbdRating:</label>
+            <input type="number" step="0.1" onChange={(event) => {
+                setRating(event.target.value)
+            }} />
+
             <p>Current Title: {title}</p>
             <p>Current Director: {director}</p>
             <p>Current Actors: {actors}</p>
             <p>Current Plot: {plot}</p>
+            <p>Current Year: {year}</p>
+            <p>Current imdbRating: {imbdRating}</p>
 
         </div>
     )
