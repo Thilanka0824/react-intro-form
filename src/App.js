154c39e80c1dd7fc8f850c91fc39b30aaab254d5
Thilanka Rodrigo <65930513+Thilanka0824@users.noreply.github.com>
2022-10-10 12:10:12 -0700

    completed some strech goals

--- src/App.js
+++ src/App.js
@@ -4,15 +4,114 @@ import './App.css';
 //import MovieDisplay from './components/movie-display.jsx'
 import { useState } from 'react'
 
+const sampleMovies = [
+  {
+    "Title": "Avatar",
+    "Year": "2009",
+    "Rated": "PG-13",
+    "Released": "18 Dec 2009",
+    "Runtime": "162 min",
+    "Genre": "Action, Adventure, Fantasy",
+    "Director": "James Cameron",
+    "Writer": "James Cameron",
+    "Actors": "Sam Worthington, Zoe Saldana, Sigourney Weaver, Stephen Lang",
+    "Plot": "A paraplegic marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.",
+    "Language": "English, Spanish",
+    "Country": "USA, UK",
+    "Awards": "Won 3 Oscars. Another 80 wins & 121 nominations.",
+    "Poster": "http://ia.media-imdb.com/images/M/MV5BMTYwOTEwNjAzMl5BMl5BanBnXkFtZTcwODc5MTUwMw@@._V1_SX300.jpg",
+    "Metascore": "83",
+    "imdbRating": "7.9",
+    "imdbVotes": "890,617",
+    "imdbID": "tt0499549",
+    "Type": "movie",
+    "Response": "True",
+    "Images": [
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMjEyOTYyMzUxNl5BMl5BanBnXkFtZTcwNTg0MTUzNA@@._V1_SX1500_CR0,0,1500,999_AL_.jpg",
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BNzM2MDk3MTcyMV5BMl5BanBnXkFtZTcwNjg0MTUzNA@@._V1_SX1777_CR0,0,1777,999_AL_.jpg",
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMTY2ODQ3NjMyMl5BMl5BanBnXkFtZTcwODg0MTUzNA@@._V1_SX1777_CR0,0,1777,999_AL_.jpg",
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMTMxOTEwNDcxN15BMl5BanBnXkFtZTcwOTg0MTUzNA@@._V1_SX1777_CR0,0,1777,999_AL_.jpg",
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMTYxMDg1Nzk1MV5BMl5BanBnXkFtZTcwMDk0MTUzNA@@._V1_SX1500_CR0,0,1500,999_AL_.jpg"
+    ]
+  },
+  {
+    "Title": "I Am Legend",
+    "Year": "2007",
+    "Rated": "PG-13",
+    "Released": "14 Dec 2007",
+    "Runtime": "101 min",
+    "Genre": "Drama, Horror, Sci-Fi",
+    "Director": "Francis Lawrence",
+    "Writer": "Mark Protosevich (screenplay), Akiva Goldsman (screenplay), Richard Matheson (novel), John William Corrington, Joyce Hooper Corrington",
+    "Actors": "Will Smith, Alice Braga, Charlie Tahan, Salli Richardson-Whitfield",
+    "Plot": "Years after a plague kills most of humanity and transforms the rest into monsters, the sole survivor in New York City struggles valiantly to find a cure.",
+    "Language": "English",
+    "Country": "USA",
+    "Awards": "9 wins & 21 nominations.",
+    "Poster": "http://ia.media-imdb.com/images/M/MV5BMTU4NzMyNDk1OV5BMl5BanBnXkFtZTcwOTEwMzU1MQ@@._V1_SX300.jpg",
+    "Metascore": "65",
+    "imdbRating": "7.2",
+    "imdbVotes": "533,874",
+    "imdbID": "tt0480249",
+    "Type": "movie",
+    "Response": "True",
+    "Images": [
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMTI0NTI4NjE3NV5BMl5BanBnXkFtZTYwMDA0Nzc4._V1_.jpg",
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMTIwMDg2MDU4M15BMl5BanBnXkFtZTYwMTA0Nzc4._V1_.jpg",
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMTc5MDM1OTU5OV5BMl5BanBnXkFtZTYwMjA0Nzc4._V1_.jpg",
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMTA0MTI2NjMzMzFeQTJeQWpwZ15BbWU2MDMwNDc3OA@@._V1_.jpg"
+    ]
+  },
+  {
+    "Title": "300",
+    "Year": "2006",
+    "Rated": "R",
+    "Released": "09 Mar 2007",
+    "Runtime": "117 min",
+    "Genre": "Action, Drama, Fantasy",
+    "Director": "Zack Snyder",
+    "Writer": "Zack Snyder (screenplay), Kurt Johnstad (screenplay), Michael Gordon (screenplay), Frank Miller (graphic novel), Lynn Varley (graphic novel)",
+    "Actors": "Gerard Butler, Lena Headey, Dominic West, David Wenham",
+    "Plot": "King Leonidas of Sparta and a force of 300 men fight the Persians at Thermopylae in 480 B.C.",
+    "Language": "English",
+    "Country": "USA",
+    "Awards": "16 wins & 42 nominations.",
+    "Poster": "http://ia.media-imdb.com/images/M/MV5BMjAzNTkzNjcxNl5BMl5BanBnXkFtZTYwNDA4NjE3._V1_SX300.jpg",
+    "Metascore": "52",
+    "imdbRating": "7.7",
+    "imdbVotes": "611,046",
+    "imdbID": "tt0416449",
+    "Type": "movie",
+    "Response": "True",
+    "Images": [
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMTMwNTg5MzMwMV5BMl5BanBnXkFtZTcwMzA2NTIyMw@@._V1_SX1777_CR0,0,1777,937_AL_.jpg",
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMTQwNTgyNTMzNF5BMl5BanBnXkFtZTcwNDA2NTIyMw@@._V1_SX1777_CR0,0,1777,935_AL_.jpg",
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMTc0MjQzOTEwMV5BMl5BanBnXkFtZTcwMzE2NTIyMw@@._V1_SX1777_CR0,0,1777,947_AL_.jpg"
+    ]
+  }
+]
+
+
 const MovieForm = (props) => {
   const [title, setTitle] = useState("");
   const [director, setDirector] = useState("");
   const [actors, setActors] = useState("");
   const [plot, setPlot] = useState("");
   const [year, setYear] = useState(0);
-  const [imbdRating, setRating] = useState(0);
+  const [imdbRating, setRating] = useState(0);
+  const [rated, setRated] = useState(0);
+  const [released, setReleased] = useState("");
+  const [genre, setGenre] = useState("");
+  const [writer, setWriter] = useState("");
+  const [language, setLanguage] = useState("")
+  const [images, setImages] = useState([])
 
   console.log(title)
+  
+  function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+
   return (
     <div>
       <h2>Movie Form</h2>
@@ -41,10 +140,47 @@ const MovieForm = (props) => {
         setYear(event.target.value)
       }} />
       <br />
-      <label>imbd Rating:</label>
+      <label>imdb Rating:</label>
       <input type="number" step="0.1" onChange={(event) => {
         setRating(event.target.value)
       }} />
+      <br />
+      <label>Rated</label>
+      <input type="text" onChange={(event) => {
+        setRated(event.target.value)
+      }} />
+      <br />
+      <label>Released</label>
+      <input type="date" onChange={(event) => {
+        setReleased(event.target.value)
+      }} />
+      <br />
+      <label>Genre</label>
+      <input type="text" onChange={(event) => {
+        setGenre(event.target.value)
+      }}/>
+      <br />
+      <label>Writer</label>
+      <input type="text" onChange={(event) => {
+        setWriter(event.target.value)
+      }} />
+      <br />
+      <label>Language</label>
+      <input type="text" onChange={(event) => {
+        setLanguage(event.target.value)
+      }}/>
+      <br />
+      <label>Images</label>
+      <input type="text" onChange={(event) => {
+        setImages(event.target.value)
+      }} /><div className="dropdown">
+        <button onClick={myFunction} className="dropbtn">Dropdown</button>
+        <div id="myDropdown" className="dropdown-content">
+          <a href="#">Select</a>
+          <a href="#">Options</a>
+
+        </div>
+      </div>
       <br />
       <button onClick={() => {
         const newMovie = {
@@ -53,19 +189,30 @@ const MovieForm = (props) => {
           actors: actors,
           plot: plot,
           year: year,
-          imbdRating: imbdRating
+          imdbRating: imdbRating
         }
         props.handleAddMovie(newMovie)
         console.log(newMovie)
 
       }}>Add Movie</button>
 
+      
+
       <p>Current Title: {title}</p>
       <p>Current Director: {director}</p>
       <p>Current Actors: {actors}</p>
       <p>Current Plot: {plot}</p>
       <p>Current Year: {year}</p>
-      <p>Current imdbRating: {imbdRating}</p>
+      {console.log(imdbRating)}
+      <p>Current imdbRating: {imdbRating}</p>
+      {console.log(imdbRating)}
+      <p>Rated: {rated}</p>
+      <p>Released: {released}</p>
+      <p>Genre: {genre}</p>
+      <p>Writer: {writer}</p>
+      <p>Language: {language}</p>
+      
+      
 
     </div>
   )
@@ -87,7 +234,7 @@ const MovieItem = (props) => {
         <p>Director: {props.movie.director}</p>
         <p>Actors: {props.movie.actors}</p>
         <p>Plot: {props.movie.plot}</p>
-        <p>Imdb Rating: {props.movie.imdb}</p>
+        <p>Imdb Rating: {props.movie.imdbRating}</p>
         <p>Year: {props.movie.year}</p>
       </div>
     )
@@ -98,6 +245,7 @@ const App = () => {
   const [movieList, setMovieList] = useState([]);
 
   const handleAddMovie = (newMovie) => {
+    console.log(newMovie)
     const movieListCopy = [...movieList, newMovie]
     setMovieList(movieListCopy)
   }
